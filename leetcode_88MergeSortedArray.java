d9f8d6e93d6d2dd727c531940bec8807fae89a87
Nhat Nguyen (Conan) <111167849+conan-nhat-nguyen@users.noreply.github.com>
2023-10-15 23:56:58 -0500

    Problem 88: Having setup and main method ready for

---
+++ leetcode_88MergeSortedArray.java
@@ -0,0 +1,21 @@
+public class leetcode_88MergeSortedArray {
+    int[] nums1 = {2,2,3, 4, 0, 0};
+    int[] nums2 = {1, 1};
+
+    int m = 4;
+    int n  = 2;
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+
+    private void insertAndShiftRight(int numToInsert, int[] array) {
+        for (int i = 0; i < array.length; i++) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
